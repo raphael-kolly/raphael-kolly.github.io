da6a00d8af4acf456afa13741ed41ff9948b9d30
Sgetxz <raphael.kolly@epfl.ch>
2023-12-26 16:57:51 +0100

    change pin

--- dashboard/flutter_service_worker.js
+++ dashboard/flutter_service_worker.js
@@ -28,10 +28,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "cc6a98c6e6f7b0905ac18797c4dd7aa6
 "icons/Icon-512.png": "d9a897e989b43e22d9d6685575c93e83",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c635ae8a49955f76c8ca10b5fc01babd",
-"/": "c635ae8a49955f76c8ca10b5fc01babd",
+"index.html": "e32fb6ce7290e865f9611b41acfc3c79",
+"/": "e32fb6ce7290e865f9611b41acfc3c79",
 "logo_landwehr.png": "dce551aa2feb706d9ce85841a0f12af8",
-"main.dart.js": "7ec031f0af6ce2b4d1b596f8b08de065",
+"main.dart.js": "f0260d762f958b96b62c11270b15d868",
 "manifest.json": "e3741e1992064c44052bb18d66945620",
 "version.json": "3d8047da9749df75e88b3481338d082a"};
 // The application shell files that are downloaded before a service worker can
